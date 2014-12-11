d5ecae94bd61c24478446f2926c7ea55e477e381
student <student@ai251lab0219m14.aicala.edmc.stu>
2014-12-10 16:11:05 -0800

    push

--- public/js/ag-controller.js
+++ public/js/ag-controller.js
@@ -2,5 +2,24 @@ var WeVidHereConfig = angular.module('WeVidHere',[]);
 WeVidHereConfig.controller{
 
 	'WeVidHereConfig',
-	['$scope']
+	['$scope',
+	'$http' ,
+	function ($scope, $http){
+		//get data by ajax
+		$http(
+			method:"GET",
+			url:"data/users.json"
+			)
+		.success(function (data, status, headers, config){
+			//do something if we got the data
+
+		}
+		);
+		//check data on submit form
+		$scope.doLogin = function(){
+			//process what user typed in
+
+		}
+	}
+	]
 };
